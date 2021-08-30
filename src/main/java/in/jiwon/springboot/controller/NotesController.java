09397871ae043788b2a77cb616a07e7269158cdb
jsong96 <56089838+jsong96@users.noreply.github.com>
2021-08-30 21:10:47 +0900

    changed the application properties and pom.xml

--- src/main/java/in/jiwon/springboot/controller/NotesController.java
+++ src/main/java/in/jiwon/springboot/controller/NotesController.java
@@ -5,10 +5,7 @@ import in.jiwon.springboot.repository.NotesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +19,9 @@ public class NotesController {
     @GetMapping("/notes")
     public ResponseEntity<List<Note>> readNotes () {
         return new ResponseEntity<List<Note>>(nRepo.findAll(), HttpStatus.OK);
+    }
+    @PostMapping("/notes")
+    public ResponseEntity<Note> createNote(@RequestBody Note note) {
+        return new ResponseEntity<Note>(nRepo.save(note), HttpStatus.CREATED);
     }
 }
--- src/main/java/in/jiwon/springboot/entity/Note.java
+++ src/main/java/in/jiwon/springboot/entity/Note.java
@@ -1,7 +1,11 @@
 package in.jiwon.springboot.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="tbl_notes")
@@ -16,6 +20,14 @@ public class Note {
     private String body;
 
     private String category;
+
+    @Column(name = "created_at", nullable = false, updatable=false)
+    @CreationTimestamp
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Date updatedAt;
 
 
 }
